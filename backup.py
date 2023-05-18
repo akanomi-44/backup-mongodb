ff244de812b202cda7770101638aef7376d7c7e6
Micheal <chinhannguyen04@gmail.com>
2023-05-19 00:39:33 +0700

    add backup archive prfix

--- backup.py
+++ backup.py
@@ -14,12 +14,15 @@ mongo_uri = os.environ.get('MONGO_URI')
 mongo_db = os.environ.get('MONGO_DB')
 
 # Backup settings
+backup_prefix = 'mongodb_backup'
 
 def backup_database():
     # Generate backup filename with timestamp
-   
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    backup_filename = f'{backup_prefix}_{timestamp}.archive'
+
     # Create the backup command
-    backup_command = f'mongodump --uri "{mongo_uri}" --db {mongo_db} --gzip'
+    backup_command = f'mongodump --uri "{mongo_uri}" --db {mongo_db} --archive={backup_filename}'
 
     # Execute the backup command
     subprocess.call(backup_command, shell=True)
